1c927b2ec73d7eedfabc04356bb7bf7dd3656832
Diogo Baeder de Paula Pinto <diogobaeder@canonical.com>
2013-02-19 11:52:24 -0300

    Removing Fabric and virtualenv stuff, back to Makefile

--- fabtasks/__init__.py
+++
@@ -1,9 +0,0 @@
-# flake8: noqa
-# Copyright 2011 Canonical Ltd.  This software is licensed under the
-# GNU Affero General Public License version 3 (see the file LICENSE).
-
-from .environment import *
-from .development import *
-
-# make sure the virtualenv is automatically activated
-setup_virtualenv()
