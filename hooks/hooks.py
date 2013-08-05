29ae0152b73c8ca05541dbf55343e73fe7ca59af
Evan Dandrea <evan.dandrea@canonical.com>
2013-08-05 14:17:41 +0100

    Always write a listen stanza, otherwise the haproxy config check will fail. Do not pass None relation IDs to subprocess.

--- hooks/hooks.py
+++ hooks/hooks.py
@@ -389,13 +389,14 @@ def create_services():
         if service.get("servers", []):
             has_servers = True
 
+    del services_dict[None]
+    write_service_config(services_dict)
+
     if not has_servers:
         log("No backend servers, exiting.")
         return
 
-    del services_dict[None]
     services_dict = apply_peer_config(services_dict)
-    write_service_config(services_dict)
     return services_dict
 
 
@@ -657,6 +658,8 @@ def notify_relation(relation, changed=False, relation_ids=None):
     default_port = 80
 
     for rid in relation_ids or get_relation_ids(relation):
+        if not rid:
+            continue
         service_names = set()
         for relation_data in relations_for_id(rid):
             if 'service_name' in relation_data:
