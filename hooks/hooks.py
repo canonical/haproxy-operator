2b990ab652a101051127686f72dad6bc42d47708
David Ames <david.ames@canonical.com>
2013-09-30 12:25:11 -0700

    Back out ev's revno 84 as this breaks several tests (make test). The if not rid also circumvents two lines down if rid is None: rid = relation_id()

--- hooks/hooks.py
+++ hooks/hooks.py
@@ -421,14 +421,13 @@ def create_services():
         if service.get("servers", []):
             has_servers = True
 
-    del services_dict[None]
-    write_service_config(services_dict)
-
     if not has_servers:
         log("No backend servers, exiting.")
         return
 
+    del services_dict[None]
     services_dict = apply_peer_config(services_dict)
+    write_service_config(services_dict)
     return services_dict
 
 
@@ -700,8 +699,6 @@ def notify_relation(relation, changed=False, relation_ids=None):
     default_port = 80
 
     for rid in relation_ids or get_relation_ids(relation):
-        if not rid:
-            continue
         service_names = set()
         if rid is None:
             rid = relation_id()
