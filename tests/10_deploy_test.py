851aee9109ba84a4bdf808e8baf92d9d27681397
Matt Bruzek matthew.bruzek@canonical.com <>
2014-01-24 10:57:20 -0600

    Fixing code review comments

--- tests/10_deploy_test.py
+++ tests/10_deploy_test.py
@@ -1,22 +1,15 @@
 #!/usr/bin/python3
 
-# This is an amulet test to test the haproxy functionality.
-# Test to see if a web page comes through haproxy.
-# Test to see if the haproxy charm relates to an apache2 charm.
-# Test to see if multiple instances IP addresses are in the config file for haproxy
+# This Amulet test deploys haproxy and related charms.
 
 import os
 import amulet
 import requests
 import base64
 
-units=2
-
 d = amulet.Deployment()
 # Add the haproxy charm to the deployment.
 d.add('haproxy')
-# Add two units of apache2 charm to the deployment.
-#d.add('apache2', units=units)
 d.add('apache2')
 
 # Get the directory this way to load the file when CWD is different.
@@ -25,8 +18,9 @@ template_path = os.path.join(path, 'default_apache.tmpl')
 # Read in the Apache2 default template file.
 with open(template_path) as f:
     template = f.read()
-    encodedTemplate = base64.b64encode(template.encode('ascii'))
-configuration = {'vhost_https_template' : encodedTemplate.decode('ascii')}
+    encodedTemplate = base64.b64encode(template.encode('utf-8'))
+# Create a dictionary with configuration values for apache2.
+configuration = {'vhost_https_template': encodedTemplate.decode('ascii')}
 # Apache2 needs a base64 encoded template to configure the web site.
 d.configure('apache2', configuration)
 
@@ -43,11 +37,13 @@ try:
     # Wait for the relation to finish the transations.
     d.sentry.wait(seconds)
 except amulet.helpers.TimeoutError:
-    message = 'The environment did not setup in %d seconds.', seconds
+    message = 'The environment did not setup in %d seconds.' % seconds
     # The SKIP status enables skip or fail the test based on configuration.
     amulet.raise_status(amulet.SKIP, msg=message)
 except:
     raise
+
+# Test that haproxy is acting as the proxy for apache2.
 
 # Get the haproxy unit.
 haproxy_unit = d.sentry.unit['haproxy/0']
@@ -57,24 +53,28 @@ page = requests.get('http://%s/index.html' % haproxy_address)
 page.raise_for_status()
 print('Successfully got the Apache2 web page through haproxy IP address.')
 
-# Get the sentry for apache and get the private IP address 
+# Test that the apache2 relation data is saved on the haproxy server.
+
+# Get the sentry for apache and get the private IP address.
 apache_unit = d.sentry.unit['apache2/0']
 # Get the relation.
 relation = apache_unit.relation('website', 'haproxy:reverseproxy')
 # Get the private address from the relation.
-print('Private address of the apache2 relation ', relation['private-address'])
 apache_private = relation['private-address']
+
+print('Private address of the apache2 relation ', apache_private)
+
 # Grep the configuration file for the private address
-output, code = haproxy_unit.run('grep %s /etc/haproxy/haproxy.cfg' % apache_private)
+output, code = haproxy_unit.run('grep %s /etc/haproxy/haproxy.cfg' %
+                                apache_private)
 if code == 0:
-    print("Found the relation private address in the haproxy configuration file!")
+    print('Found the relation IP address in the haproxy configuration file!')
     print(output)
 else:
     print(output)
-    message = "Unable to find the Apache IP address %s in the haproxy " \
-              "configuration file." % apache_private
+    message = 'Unable to find the Apache IP address %s in the haproxy ' \
+              'configuration file.' % apache_private
     amulet.raise_status(amulet.FAIL, msg=message)
 
 # Send a message that the tests are complete.
 print('The haproxy tests are complete.')
-
