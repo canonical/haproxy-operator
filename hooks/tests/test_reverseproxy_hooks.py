ebc50e6e4ee1a7bd62c1b9a170a472db70d409c8
Bjorn Tillenius <bjorn@tillenius.me>
2015-05-15 17:31:41 +0300

    Add tests for no backends in old and new configs.

--- hooks/tests/test_reverseproxy_hooks.py
+++ hooks/tests/test_reverseproxy_hooks.py
@@ -505,6 +505,32 @@ class ReverseProxyRelationTest(TestCase):
         self.assertRaises(
             hooks.InvalidRelationDataError, hooks.merge_service, s1, s2)
 
+    def test_merge_service_no_old_backend(self):
+        """
+        If the old service config has no backends, the backends from the
+        new config is used..
+        """
+
+        s1 = {}
+        s2 = {'backends': [
+            {'backend_name': 'webapp',
+             'servers': [['webapp-1', '10.0.0.2', 8090, []]]},
+        ]}
+        self.assertEqual(s2, hooks.merge_service(s1, s2))
+
+    def test_merge_service_no_new_backend(self):
+        """
+        If the new service config has no backends, the backends from the
+        old config is used..
+        """
+
+        s1 = {'backends': [
+            {'backend_name': 'webapp',
+             'servers': [['webapp-1', '10.0.0.2', 8090, []]]},
+        ]}
+        s2 = {}
+        self.assertEqual(s1, hooks.merge_service(s1, s2))
+
     def test_merge_service_backend_name_matching(self):
         """Backends are merged by backend_name."""
 
