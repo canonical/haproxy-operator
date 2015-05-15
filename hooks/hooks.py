81c0e0b83eaeea425dc949cf9e7945e29203810b
Danilo Šegan <danilo@canonical.com>
2015-05-15 21:30:49 +0800

    Refactor merge_service and improve test.

--- hooks/hooks.py
+++ hooks/hooks.py
@@ -12,6 +12,7 @@ import yaml
 import pwd
 
 from itertools import izip, tee, groupby
+from operator import itemgetter
 
 from charmhelpers.core.host import pwgen, lsb_release
 from charmhelpers.core.hookenv import (
@@ -496,6 +497,19 @@ def parse_services_yaml(services, yaml_data):
     return services
 
 
+def _add_items_if_missing(target, additions):
+    """
+    Append items from `additions` to `target` if they are not present already.
+
+    Returns a new list.
+    """
+    result = target[:]
+    for addition in additions:
+        if addition not in result:
+           result.append(addition)
+    return result
+
+
 def merge_service(old_service, new_service):
     """
     Helper function to merge two service entries correctly.
@@ -504,22 +518,44 @@ def merge_service(old_service, new_service):
     """
     service = new_service.copy()
     service.update(old_service)
+
+    # Merge all 'servers' entries of the default backend.
+    if "servers" in old_service and "servers" in new_service:
+        service["servers"] = _add_items_if_missing(
+            service["servers"], new_service["servers"])
+
+    # We sort results for higher stability.
     if "servers" in service:
-        # Merge all 'servers' entries of the default backend
-        servers = service["servers"]
-        if "servers" in new_service:
-            servers.extend(new_service["servers"])
-            servers.sort()
-            service["servers"] = list(x for x, _ in groupby(servers))
-    if "backends" in service and "backends" in new_service:
-        # Merge all 'servers' entries of the additional backends
-        for i, backend in enumerate(service["backends"]):
-            servers = backend["servers"]
-            if len(new_service["backends"]) > i:
-                servers.extend(new_service["backends"][i]["servers"])
-            servers.sort()
-            service["backends"][i]["servers"] = list(
-                x for x, _ in groupby(servers))
+        service["servers"].sort()
+
+    # Merge all 'backends' and their contained "servers".
+    if "backends" in old_service and "backends" in new_service:
+        backends_by_name = {}
+        # Populate backends_by_name with backends from (old_)service.
+        for backend in service["backends"]:
+            backend_name = backend.get("backend_name")
+            if backend_name is None:
+                raise Exception("Each backend must have backend_name.")
+            backends_by_name.setdefault(backend_name, backend)
+
+        # Go through backends in new_service and add them to backends_by_name,
+        # merging 'servers' while at it.
+        for backend in new_service["backends"]:
+            backend_name = backend.get("backend_name")
+            if backend_name is None:
+                raise Exception("Each backend must have backend_name.")
+            if backend_name in backends_by_name:
+                # Merge servers.
+                target_backend = backends_by_name[backend_name]
+                target_backend["servers"] = sorted(
+                    _add_items_if_missing(
+                        target_backend["servers"], backend["servers"]))
+            else:
+                backend["servers"].sort()
+                backends_by_name[backend_name] = backend
+
+        service["backends"] = sorted(
+            backends_by_name.values(), key=itemgetter('backend_name'))
     return service
 
 
--- hooks/tests/test_reverseproxy_hooks.py
+++ hooks/tests/test_reverseproxy_hooks.py
@@ -489,27 +489,29 @@ class ReverseProxyRelationTest(TestCase):
         expected = {'service_name': 'left', 'foo': 'bar', 'bar': 'baz'}
         self.assertEqual(expected, hooks.merge_service(s1, s2))
 
-    def test_merge_service_bug(self):
+    def test_merge_service_backend_name_matching(self):
         """ Make sure merge_services maintains "server" entries. """
 
         s1 = {'backends': [
-            {'backend_name': 'webapp',
-             'servers': [['webapp-0', '10.0.3.170', 8090, []]]},
             {'backend_name': 'api',
-             'servers': [['api-0', '10.0.3.170', 9080, []]]}]}
+             'servers': [['api-0', '10.0.0.1', 9080, []]]},
+            {'backend_name': 'webapp',
+             'servers': [['webapp-0', '10.0.0.1', 8090, []]]},
+        ]}
         s2 = {'backends': [
             {'backend_name': 'webapp',
-             'servers': [['webapp-1', '10.0.3.121', 8090, []]]}]}
+             'servers': [['webapp-1', '10.0.0.2', 8090, []]]},
+        ]}
         expected = {
             'backends': [
                 {'backend_name': 'api',
-                 'servers': [['api-0', '10.0.3.121', 9080, []]]},
+                 'servers': [['api-0', '10.0.0.1', 9080, []]]},
                 {'backend_name': 'webapp',
-                 'servers': [['webapp-0', '10.0.3.170', 8090, []],
-                             ['webapp-1', '10.0.3.121', 8090, []]]},
+                 'servers': [['webapp-0', '10.0.0.1', 8090, []],
+                             ['webapp-1', '10.0.0.2', 8090, []]]},
             ]
         }
-        self.assertItemsEqual(expected, hooks.merge_service(s1, s2))
+        self.assertEqual(expected, hooks.merge_service(s1, s2))
 
     def test_join_reverseproxy_relation(self):
         """
