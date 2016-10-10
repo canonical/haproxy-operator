0e358bfe9f1d6e42f64447e77a33f4c0d335450b
Martin Hilton <martin.hilton@canonical.com>
2016-10-10 13:52:37 +0100

    Update check_haproxy NRPE check to support https services

--- files/nrpe/check_haproxy.sh
+++ files/nrpe/check_haproxy.sh
@@ -3,31 +3,38 @@
 # This file is managed by Juju
 #--------------------------------------------
 #
-# Copyright 2009,2012 Canonical Ltd.
-# Author: Tom Haddon
+# Copyright 2009,2012,2016 Canonical Ltd.
+# Author: Tom Haddon, Junien Fridrick
+
+set -e
 
 CRITICAL=0
 NOTACTIVE=''
 LOGFILE=/var/log/nagios/check_haproxy.log
-AUTH=$(grep -r "stats auth" /etc/haproxy | head -1 | awk '{print $4}')
+AUTH=$(grep "stats auth" /etc/haproxy/haproxy.cfg | head -1 | awk '{print $4}')
+SSL=$(grep 10000 /etc/haproxy/haproxy.cfg | grep -q ssl && echo "-S" || true)
+HAPROXY_SOCKET=/var/run/haproxy.user.sock
 
-HAPROXY_VERSION="$(dpkg-query -W --showformat='${Version}\n' haproxy)"
-if dpkg --compare-versions "$HAPROXY_VERSION" ge 1.5; then
-    CLASS_REGEX='(active_up|backup_up)'
-else
-    CLASS_REGEX='(active|backup)(2|3)'
-fi
+# columns with service name and service status in the CSV output
+NAME_COL=2
+STATUS_COL=18
 
-for appserver in $(grep '    server' /etc/haproxy/haproxy.cfg | awk '{print $2'});
-do
-    output=$(/usr/lib/nagios/plugins/check_http -a ${AUTH} -I 127.0.0.1 -p 10000 --regex="class=\"$CLASS_REGEX.*${appserver}" -e ' 200 OK')
-    if [ $? != 0 ]; then
-        date >> $LOGFILE
-        echo $output >> $LOGFILE
-        /usr/lib/nagios/plugins/check_http -a ${AUTH} -I 127.0.0.1 -p 10000 -v | grep $appserver >> $LOGFILE 2>&1
-        CRITICAL=1
-        NOTACTIVE="${NOTACTIVE} $appserver"
-    fi
+for line in $(echo 'show stat'|socat $HAPROXY_SOCKET stdio|egrep -v '^#'|cut -d, -f$NAME_COL,$STATUS_COL); do
+    IFS=','
+    set $line
+    appserver=$1
+    state=$2
+    case $appserver in
+       # ignore the FRONTEND and BACKEND servers
+        FRONTEND|BACKEND) continue;;
+        *) if [ "$state" != "UP" ]; then
+               date >> $LOGFILE
+               echo "Server $appserver is in status $state" >> $LOGFILE
+               /usr/lib/nagios/plugins/check_http ${SSL} -a ${AUTH} -I 127.0.0.1 -p 10000 -v | grep $appserver >> $LOGFILE 2>&1
+               CRITICAL=1
+               NOTACTIVE="${NOTACTIVE} $appserver"
+          fi
+    esac
 done
 
 if [ $CRITICAL = 1 ]; then
