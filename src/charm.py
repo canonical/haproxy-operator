3b584fd146837751fb61a333940864925fa70ee2
Thanhphan1147 <trung.thanh.phan@canonical.com>
2024-08-05 18:03:44 +0200

    validate config with decorator

--- src/charm.py
+++ src/charm.py
@@ -13,7 +13,8 @@ import typing
 import ops
 
 import haproxy
-from state.config import CharmConfig, InvalidCharmConfigError
+from state.config import CharmConfig
+from state.validation import validate_config_and_integration
 
 logger = logging.getLogger(__name__)
 
@@ -44,15 +45,10 @@ class HAProxyCharm(ops.CharmBase):
             raise RuntimeError("Service not running.")
         self.unit.status = ops.ActiveStatus()
 
+    @validate_config_and_integration
     def _on_config_changed(self, _: typing.Any) -> None:
         """Handle the config-changed event."""
-        try:
-            config = CharmConfig.from_charm(self)
-        except InvalidCharmConfigError as exc:
-            logger.exception("Error initializing the charm state.")
-            self.unit.status = ops.BlockedStatus(exc.msg)
-            return
-
+        config = CharmConfig.from_charm(self)
         self.haproxy_service.render_haproxy_config(config)
 
 
