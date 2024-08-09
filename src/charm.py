266659c6d6ebf4388e8c22d588c624f998240dc8
Thanhphan1147 <trung.thanh.phan@canonical.com>
2024-08-09 18:51:10 +0200

    update tls management

--- src/charm.py
+++ src/charm.py
@@ -11,12 +11,21 @@ import logging
 import typing
 
 import ops
+from charms.tls_certificates_interface.v3.tls_certificates import (
+    CertificateAvailableEvent,
+    TLSCertificatesRequiresV3,
+)
+from ops.charm import ActionEvent
 
 import haproxy
 from state.config import CharmConfig
+from state.tls import TLSInformation
 from state.validation import validate_config_and_integration
+from tls_relation import TLSRelationService
 
 logger = logging.getLogger(__name__)
+
+TLS_CERT_RELATION = "certificates"
 
 
 class HAProxyCharm(ops.CharmBase):
@@ -30,6 +39,10 @@ class HAProxyCharm(ops.CharmBase):
         """
         super().__init__(*args)
         self.haproxy_service = haproxy.HAProxyService()
+
+        self.certificates = TLSCertificatesRequiresV3(self, TLS_CERT_RELATION)
+        self._tls = TLSRelationService(self.model, self.certificates)
+
         self.framework.observe(self.on.install, self._on_install)
         self.framework.observe(self.on.config_changed, self._on_config_changed)
 
@@ -38,11 +51,51 @@ class HAProxyCharm(ops.CharmBase):
         self.haproxy_service.install()
         self.unit.status = ops.ActiveStatus()
 
-    @validate_config_and_integration
     def _on_config_changed(self, _: typing.Any) -> None:
         """Handle the config-changed event."""
+        self._reconcile_certificates()
+        self._reconcile()
+
+    @validate_config_and_integration(defer=False)
+    def _on_certificate_available(self, _: CertificateAvailableEvent) -> None:
+        """Handle the TLS Certificate available event."""
+        self._reconcile()
+
+    @validate_config_and_integration(defer=False)
+    def _on_get_certificate_action(self, event: ActionEvent) -> None:
+        """Triggered when users run the `get-certificate` Juju action.
+
+        Args:
+            event: Juju event
+        """
+        TLSInformation.validate_certificates_integration(self)
+
+        hostname = event.params["hostname"]
+        if provider_cert := self._tls.get_provider_cert_with_hostname(hostname):
+            event.set_results(
+                {
+                    "certificate": provider_cert.certificate,
+                    "ca": provider_cert.ca,
+                    "chain": provider_cert.chain_as_pem(),
+                }
+            )
+            return
+
+        event.fail(f"Missing or incomplete certificate data for {hostname}")
+
+    @validate_config_and_integration(defer=False)
+    def _reconcile(self) -> None:
+        """Render the haproxy config and restart the service."""
         config = CharmConfig.from_charm(self)
         self.haproxy_service.render_haproxy_config(config)
+
+    @validate_config_and_integration(defer=False)
+    def _reconcile_certificates(self) -> None:
+        """Request new certificates if needed to match the configured hostname."""
+        tls_information = TLSInformation.from_charm(self, self.certificates)
+        if not self._tls.get_provider_cert_with_hostname(tls_information.external_hostname):
+            self._tls.generate_private_key(tls_information.external_hostname)
+            self._tls.request_certificate(tls_information.external_hostname)
 
 
 if __name__ == "__main__":  # pragma: nocover
