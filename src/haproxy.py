3cb312ec3c114e553968b0f0b4bd8e03616d4b6f
Thanhphan1147 <trung.thanh.phan@canonical.com>
2024-08-09 19:06:46 +0200

    enable and start the haproxy service

--- src/haproxy.py
+++ src/haproxy.py
@@ -8,6 +8,8 @@ import os
 import pwd
 from pathlib import Path
 
+import logging
+
 from charms.operator_libs_linux.v0 import apt
 from charms.operator_libs_linux.v1 import systemd
 from jinja2 import Template
