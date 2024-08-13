9867f9f508e781474a9659acd7d91cabbe53d38b
Thanhphan1147 <trung.thanh.phan@canonical.com>
2024-08-13 21:35:30 +0200

    add unit tests for tls_relation

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -2,7 +2,7 @@
 # See LICENSE file for licensing details.
 
 """Fixtures for haproxy-operator unit tests."""
-from unittest.mock import MagicMock, PropertyMock
+from unittest.mock import MagicMock
 
 import pytest
 from ops.model import Secret
