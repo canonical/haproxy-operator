922983bc4f5a08df57890a6718acce3acb03c1f9
Thanhphan1147 <trung.thanh.phan@canonical.com>
2024-08-21 00:44:07 +0200

    remove unused method

--- src/haproxy.py
+++ src/haproxy.py
@@ -62,20 +62,6 @@ class HAProxyService:
         if not self.is_active():
             raise RuntimeError("HAProxy service is not running.")
 
-    def enable_haproxy_service(self) -> None:
-        """Enable and start the haporxy service if it is not running.
-
-        Raises:
-            HaproxyServiceStartError: If the haproxy service cannot be enabled and started.
-        """
-        try:
-            systemd.service_enable(HAPROXY_SERVICE)
-            if not systemd.service_running(HAPROXY_SERVICE):
-                systemd.service_start(HAPROXY_SERVICE)
-        except systemd.SystemdError as exc:
-            logger.exception("Error starting the haproxy service")
-            raise HaproxyServiceStartError("Error starting the haproxy service") from exc
-
     def restart_haproxy_service(self) -> None:
         """Restart the haporxy service."""
         systemd.service_restart(HAPROXY_SERVICE)
