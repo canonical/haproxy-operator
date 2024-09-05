90da2b40cc148253dddc6ebaa6604ceb499f89be
Thanhphan1147 <trung.thanh.phan@canonical.com>
2024-09-05 07:37:39 +0200

    format code

--- src/charm.py
+++ src/charm.py
@@ -225,7 +225,7 @@ class HAProxyCharm(ops.CharmBase):
             self._tls.generate_private_key(tls_information.external_hostname)
             self._tls.request_certificate(tls_information.external_hostname)
 
-    @validate_config_and_tls(defer=True, block_on_tls_not_ready=True)
+    @validate_config_and_tls(defer=False, block_on_tls_not_ready=True)
     def _on_ingress_data_provided(self, event: IngressPerAppDataProvidedEvent) -> None:
         """Handle the data-provided event.
 
@@ -241,7 +241,7 @@ class HAProxyCharm(ops.CharmBase):
             event.relation, f"https://{tls_information.external_hostname}/{path_prefix}/"
         )
 
-    @validate_config_and_tls(defer=False, block_on_tls_not_ready=True)
+    @validate_config_and_tls(defer=False, block_on_tls_not_ready=False)
     def _on_ingress_data_removed(self, _: IngressPerAppDataRemovedEvent) -> None:
         """Handle the data-removed event."""
         self._reconcile()
