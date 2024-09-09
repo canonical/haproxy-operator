8a4cf55e549c8537ad66ca91a67782f8df5d7b7e
Thanhphan1147 <trung.thanh.phan@canonical.com>
2024-09-09 20:18:23 +0200

    update integration test

---
+++ tests/integration/test_ingress.py
@@ -0,0 +1,38 @@
+# Copyright 2024 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+"""Integration test for actions."""
+
+import pytest
+import requests
+from juju.application import Application
+from juju.client._definitions import FullStatus, UnitStatus
+
+@pytest.mark.abort_on_fail
+async def test_ingress_integration(application: Application, any_charm_ingress_requirer: Application):
+    """Deploy the charm with valid config and tls integration.
+
+    Assert on valid output of get-certificate.
+    """
+    action = await any_charm_ingress_requirer.units[0].run_action(
+        "rpc",
+        method="start_server",
+    )
+    await action.wait()
+    await application.model.add_relation(
+        f"{application.name}:ingress", any_charm_ingress_requirer.name
+    )
+    
+    await application.model.wait_for_idle(
+        apps=[application.name],
+        idle_period=30,
+        status="active",
+    )
+
+    status: FullStatus = await application.model.get_status([application.name])
+    unit_status: UnitStatus = next(
+        iter(status.applications[application.name].units.values())
+    )
+    assert unit_status.address, "Invalid unit address"    
+    response = requests.get(f"http://unit_status.address/{any_charm_ingress_requirer.model.name}-{any_charm_ingress_requirer.name}", timeout=5)
+    assert response.status_code == 200
