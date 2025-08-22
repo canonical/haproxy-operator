d411a531d5e8ca321bf2550c892b5b24955444aa
Thanhphan1147 <trung.thanh.phan@canonical.com>
2025-08-22 19:07:53 +0200

    add nl, add explanation for lint ignores

--- tests/integration/haproxy_route_tcp_requirer.py
+++ tests/integration/haproxy_route_tcp_requirer.py
@@ -5,6 +5,7 @@
 """haproxy-route requirer source."""
 
 import logging
+# Ignoring import subprocess warning as we're using it with no user inputs
 import subprocess  # nosec
 import textwrap
 from pathlib import Path
@@ -13,7 +14,6 @@ import ops
 from any_charm_base import AnyCharmBase  # type: ignore
 from haproxy_route_tcp import HaproxyRouteTcpRequirer, TCPHealthCheckType  # type: ignore
 
-TLS_CERT_RELATION = "require-tls-certificates:"
 HAPROXY_ROUTE_TCP_RELATION = "require-haproxy-route-tcp"
 
 TLS_ROOT = "/var/snap/ping-pong-tcp/common/"
@@ -28,6 +28,7 @@ class AnyCharm(AnyCharmBase):
     """haproxy-route requirer charm."""
 
     def __init__(self, *args, **kwargs):
+        # We don't need to include *args and *kwargs in the docstring here.
         """Initialize the requirer charm."""  # noqa
         super().__init__(*args, **kwargs)
         self._haproxy_route_tcp = HaproxyRouteTcpRequirer(self, HAPROXY_ROUTE_TCP_RELATION)
@@ -58,17 +59,20 @@ class AnyCharm(AnyCharmBase):
             "snap install ping-pong-tcp; "
             f"snap set ping-pong-tcp host=0.0.0.0"
         )
+        # Ignoring subprocess warnings as we're using it with no user inputs
         subprocess.check_output(["/bin/bash", "-c", command])  # nosec
         self.unit.status = ops.ActiveStatus("TCP server ready (TCP).")
 
     def start_tls(self):
         """Start server in TLS mode."""
         command = f"snap set ping-pong-tcp tls.cert={CERT_PATH} tls.key={KEY_PATH}"
+        # Ignoring subprocess warnings as we're using it with no user inputs
         subprocess.check_output(["/bin/bash", "-c", command])  # nosec
         self.unit.status = ops.ActiveStatus("TCP server ready (TLS).")
 
     def start_tcp(self):
         """Start server in plaintext mode."""
+        # Ignoring subprocess warnings as we're using it with no user inputs
         subprocess.check_output(
             ["/bin/bash", "-c", "snap unset ping-pong-tcp tls.cer tls.key"]
         )  # nosec
