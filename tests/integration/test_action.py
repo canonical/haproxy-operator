f3e7cd8d7a6f3fa3d445075428615b92cdb28b7d
Fouad Atwi <fouad.atwi@canonical.com>
2025-10-14 11:41:09 +0200

    add integration tests

---
+++ tests/integration/test_action.py
@@ -0,0 +1,92 @@
+# Copyright 2025 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+"""Integration tests for the ingress per unit relation."""
+
+
+import json
+
+import jubilant
+import pytest
+
+
+@pytest.mark.abort_on_fail
+def test_get_proxied_endpoints_action(
+    configured_application_with_tls: str,
+    any_charm_haproxy_route_requirer: str,
+    juju: jubilant.Juju,
+):
+    """Deploy the charm with anycharm ingress per unit requirer that installs apache2.
+
+    Assert that the requirer endpoints are available.
+    """
+    juju.integrate(
+        f"{configured_application_with_tls}:haproxy-route", any_charm_haproxy_route_requirer
+    )
+
+    juju.run(
+        f"{any_charm_haproxy_route_requirer}/0",
+        "rpc",
+        {
+            "method": "update_relation",
+            "args": json.dumps(
+                [
+                    {
+                        "service": "any_charm",
+                        "ports": [80],
+                        "hostname": "ok.haproxy.internal",
+                        "additional_hostnames": ["ok2.haproxy.internal", "ok3.haproxy.internal"],
+                        "paths": ["v1", "v2"],
+                    }
+                ]
+            ),
+        },
+    )
+    juju.wait(
+        lambda status: jubilant.all_active(
+            status, configured_application_with_tls, any_charm_haproxy_route_requirer
+        )
+    )
+    task = juju.run(f"{configured_application_with_tls}/0", "get-proxied-endpoints")
+
+    assert task.results == {
+        "endpoints": json.dumps(
+            [
+                "https://ok.haproxy.internal/v1",
+                "https://ok.haproxy.internal/v2",
+                "https://ok2.haproxy.internal/v1",
+                "https://ok2.haproxy.internal/v2",
+                "https://ok3.haproxy.internal/v1",
+                "https://ok3.haproxy.internal/v2",
+            ]
+        )
+    }, task.results
+
+    task = juju.run(
+        f"{configured_application_with_tls}/0", "get-proxied-endpoints", {"backend": "any_charm"}
+    )
+
+    assert task.results == {
+        "endpoints": json.dumps(
+            [
+                "https://ok.haproxy.internal/v1",
+                "https://ok.haproxy.internal/v2",
+                "https://ok2.haproxy.internal/v1",
+                "https://ok2.haproxy.internal/v2",
+                "https://ok3.haproxy.internal/v1",
+                "https://ok3.haproxy.internal/v2",
+            ]
+        )
+    }, task.results
+
+    with pytest.raises(jubilant.TaskError) as excinfo:
+        task = juju.run(
+            f"{configured_application_with_tls}/0",
+            "get-proxied-endpoints",
+            {"backend": "other_charm"},
+        )
+    assert 'No backend with name "other_charm"' in str(excinfo.value)
+
+    juju.remove_relation(
+        f"{configured_application_with_tls}:haproxy-route", any_charm_haproxy_route_requirer
+    )
