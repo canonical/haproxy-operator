aacbae867e20b7454d0a3672aa43120150df9d42
Javier de la Puente <javier.delapuente@canonical.com>
2025-11-20 10:00:45 +0100

    suggestions from code review

--- terraform/tests/main.tf
+++ terraform/tests/main.tf
@@ -1,22 +1,10 @@
 # Copyright 2025 Canonical Ltd.
 # See LICENSE file for licensing details.
 
-variable "channel" {
-  description = "The channel to use when deploying a charm."
-  type        = string
-  default     = "latest/edge"
-}
-
-variable "revision" {
-  description = "Revision number of the charm."
-  type        = number
-  default     = null
-}
-
 terraform {
   required_providers {
     juju = {
-      version = "~> 0.20.0"
+      version = "~> 1.0"
       source  = "juju/juju"
     }
   }
