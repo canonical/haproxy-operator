ee9148274946c5744effa469ee91298767be2617
Javier de la Puente <javier.delapuente@canonical.com>
2025-12-04 14:40:25 +0100

    start to move things around

---
+++ tests/integration/conftest.py
@@ -0,0 +1,11 @@
+# Copyright 2025 Canonical Ltd.
+# See LICENSE file for licensing details.
+# pylint: disable=duplicate-code
+
+"""Fixtures for haproxy charm integration tests."""
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+TEST_EXTERNAL_HOSTNAME_CONFIG = "haproxy.internal"
