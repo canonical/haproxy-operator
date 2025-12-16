d06631b8815ed1a3ef8519f61e7b0431c292da48
Javier de la Puente <javier.delapuente@canonical.com>
2025-12-16 16:08:52 +0100

    Add enable-hsts configuration option

--- haproxy-operator/tests/unit/test_hsts.py
+++ haproxy-operator/tests/unit/test_hsts.py
@@ -17,18 +17,48 @@ from .helper import RegexMatcher
 
 
 @pytest.mark.usefixtures("systemd_mock", "mocks_external_calls")
-def test_hsts_enabled(monkeypatch: pytest.MonkeyPatch, certificates_integration):
+def test_hsts_disabled(monkeypatch: pytest.MonkeyPatch, certificates_integration):
     """
     arrange: Prepare a haproxy with haproxy_route.
     act: trigger relation changed.
-    assert: haproxy.conf will set the strict-transport-security response header but not for the allow_http domain.
+    assert: haproxy.conf will not set the strict-transport-security response header.
     """
     render_file_mock = MagicMock()
     monkeypatch.setattr("haproxy.render_file", render_file_mock)
     haproxy_route_relation = build_haproxy_route_relation()
 
     ctx = ops.testing.Context(HAProxyCharm)
-    state = ops.testing.State(relations=[certificates_integration, haproxy_route_relation])
+    state = ops.testing.State(
+        relations=[certificates_integration, haproxy_route_relation],
+    )
+    out = ctx.run(
+        ctx.on.relation_changed(haproxy_route_relation),
+        state,
+    )
+    assert render_file_mock.call_count == 1
+    assert (
+        "http-response set-header Strict-Transport-Security"
+        not in render_file_mock.call_args.args[1]
+    )
+    assert out.unit_status.name == ops.testing.ActiveStatus.name
+
+
+@pytest.mark.usefixtures("systemd_mock", "mocks_external_calls")
+def test_hsts_enabled(monkeypatch: pytest.MonkeyPatch, certificates_integration):
+    """
+    arrange: Prepare a haproxy with haproxy_route and hsts enabled in the config option.
+    act: trigger relation changed.
+    assert: haproxy.conf will set the strict-transport-security response header.
+    """
+    render_file_mock = MagicMock()
+    monkeypatch.setattr("haproxy.render_file", render_file_mock)
+    haproxy_route_relation = build_haproxy_route_relation()
+
+    ctx = ops.testing.Context(HAProxyCharm)
+    state = ops.testing.State(
+        relations=[certificates_integration, haproxy_route_relation],
+        config={"enable-hsts": True},
+    )
     out = ctx.run(
         ctx.on.relation_changed(haproxy_route_relation),
         state,
@@ -37,7 +67,7 @@ def test_hsts_enabled(monkeypatch: pytest.MonkeyPatch, certificates_integration)
     render_file_mock.assert_any_call(
         pathlib.Path("/etc/haproxy/haproxy.cfg"),
         RegexMatcher(
-            'http-response set-header Strict-Transport-Security "max-age=16000000; includeSubDomains; preload;" if { ssl_fc } \n'
+            'http-response set-header Strict-Transport-Security "max-age=2592000;" if { ssl_fc } \n'
         ),
         ANY,
     )
@@ -47,7 +77,7 @@ def test_hsts_enabled(monkeypatch: pytest.MonkeyPatch, certificates_integration)
 @pytest.mark.usefixtures("systemd_mock", "mocks_external_calls")
 def test_hsts_disabled_allow_http(monkeypatch: pytest.MonkeyPatch, certificates_integration):
     """
-    arrange: Prepare a haproxy with haproxy_route with one allow_http domain.
+    arrange: Prepare a haproxy with haproxy_route with one allow_http domain and hsts enabled.
     act: trigger relation changed.
     assert: haproxy.conf will set the strict-transport-security response header but not for the allow_http domain.
     """
@@ -57,7 +87,10 @@ def test_hsts_disabled_allow_http(monkeypatch: pytest.MonkeyPatch, certificates_
     haproxy_route_relation.remote_app_data["allow_http"] = "true"
 
     ctx = ops.testing.Context(HAProxyCharm)
-    state = ops.testing.State(relations=[certificates_integration, haproxy_route_relation])
+    state = ops.testing.State(
+        relations=[certificates_integration, haproxy_route_relation],
+        config={"enable-hsts": True},
+    )
     out = ctx.run(
         ctx.on.relation_changed(haproxy_route_relation),
         state,
@@ -67,7 +100,7 @@ def test_hsts_disabled_allow_http(monkeypatch: pytest.MonkeyPatch, certificates_
         pathlib.Path("/etc/haproxy/haproxy.cfg"),
         RegexMatcher(
             re.escape(
-                'http-response set-header Strict-Transport-Security "max-age=16000000; includeSubDomains; preload;" if { ssl_fc }  !{ req.hdr(Host) -m str haproxy.internal }\n'
+                'http-response set-header Strict-Transport-Security "max-age=2592000;" if { ssl_fc }  !{ req.hdr(Host) -m str haproxy.internal }\n'
             )
         ),
         ANY,
