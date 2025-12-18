22ebc95747d9ab66625fa2e2903cd2b559c7c7ac
Fouad Atwi <fouad.atwi@canonical.com>
2025-12-18 12:32:49 +0100

    Update haproxy-operator/tests/unit/test_state.py
    Co-authored-by: Sébastien Georget <sebastien.georget@canonical.com>

--- haproxy-operator/tests/unit/test_state.py
+++ haproxy-operator/tests/unit/test_state.py
@@ -497,7 +497,7 @@ def test_tcp_only_happy_path(
     assert len(haproxy_route_information.valid_tcp_endpoints()) == 1
 
 
-def test_tcp_http_no_conflict_no_tcp_endpoints(
+def test_http_only_happy_path(
     haproxy_route_relation_data: typing.Callable[..., HaproxyRouteRequirerData],
 ) -> None:
     """
