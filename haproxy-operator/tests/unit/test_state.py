5c02966c65ab7a85130a2f62e0dbff990d1316c1
swetha1654 <swetha.swaminathan@canonical.com>
2025-12-19 15:16:28 +0530

    lint fix

--- haproxy-operator/tests/unit/test_state.py
+++ haproxy-operator/tests/unit/test_state.py
@@ -23,6 +23,7 @@ from charms.traefik_k8s.v1.ingress_per_unit import (
     DataValidationError as V1DataValidationError,
 )
 from charms.traefik_k8s.v2.ingress import DataValidationError as V2DataValidationError
+
 from state.charm_state import CharmState, ProxyMode
 from state.haproxy_route import HaproxyRouteRequirersInformation
 from state.haproxy_route_tcp import HAProxyRouteTcpEndpoint
