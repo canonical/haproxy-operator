7279f990f120eefc22b1b54414b1165fa162a7ee
swetha1654 <swetha.swaminathan@canonical.com>
2026-01-16 12:30:32 +0530

    Add terraform modules

---
+++ terraform/charm/haproxy_ddos_protection_configurator/main.tf
@@ -0,0 +1,28 @@
+# Copyright 2025 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+terraform {
+  required_providers {
+    juju = {
+      source  = "juju/juju"
+      version = "~> 1.0"
+    }
+  }
+  required_version = "~> 1.6"
+}
+
+resource "juju_application" "haproxy_ddos_protection_configurator" {
+  name       = var.app_name
+  model_uuid = var.model_uuid
+  units      = var.units
+
+  charm {
+    name     = "haproxy-ddos-protection-configurator"
+    revision = var.revision
+    channel  = var.channel
+    base     = var.base
+  }
+
+  config      = var.config
+  constraints = var.constraints
+}
