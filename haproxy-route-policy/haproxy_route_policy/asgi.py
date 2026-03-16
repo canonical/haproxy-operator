c6aa6f357c6dac043ed355195db1b4d4a0857548
tphan025 <trung.thanh.phan@canonical.com>
2026-03-16 02:16:14 +0100

    add licence header

--- haproxy-route-policy/haproxy_route_policy/asgi.py
+++ haproxy-route-policy/haproxy_route_policy/asgi.py
@@ -1,3 +1,6 @@
+# Copyright 2026 Canonical Ltd.
+# See LICENSE file for licensing details.
+
 """
 ASGI config for haproxy_route_policy project.
 
--- haproxy-route-policy/manage.py
+++ haproxy-route-policy/manage.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python
+
+# Copyright 2026 Canonical Ltd.
+# See LICENSE file for licensing details.
+
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
