eb246c38c89ba533c53c3b39b0f3aa81abd70bc8
Phan Trung Thanh <trung.thanh.phan@canonical.com>
2026-03-16 09:58:39 +0100

    bootstrap policy app with django-admin startproject (#388)
    * bootstrap policy app with django-admin startproject
    
    * add licence header

---
+++ haproxy-route-policy/haproxy_route_policy/asgi.py
@@ -0,0 +1,19 @@
+# Copyright 2026 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+"""
+ASGI config for haproxy_route_policy project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'haproxy_route_policy.settings')
+
+application = get_asgi_application()
