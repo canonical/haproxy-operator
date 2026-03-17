6e98d08bff7ed9cb1a0dbb8d2b5dc8ba152d41b6
tphan025 <trung.thanh.phan@canonical.com>
2026-03-17 19:16:19 +0100

    ruff format

--- haproxy-route-policy/haproxy_route_policy/wsgi.py
+++ haproxy-route-policy/haproxy_route_policy/wsgi.py
@@ -14,6 +14,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'haproxy_route_policy.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "haproxy_route_policy.settings")
 
 application = get_wsgi_application()
