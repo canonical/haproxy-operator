fb9e14324a8f8c72dc47a8a017ae41d84eced960
tphan025 <trung.thanh.phan@canonical.com>
2026-03-19 01:58:54 +0100

    update tests and move validation to serializer class

--- haproxy-route-policy/policy/db_models.py
+++ haproxy-route-policy/policy/db_models.py
@@ -82,11 +82,6 @@ class BackendRequest(models.Model):
     updated_at: models.DateTimeField = models.DateTimeField(auto_now=True)
 
 
-def is_valid_path(value: typing.Any):
-    """Validate that the value is a list of valid URL paths."""
-    return not isinstance(value, str) or not value.startswith("/")
-
-
 class Rule(models.Model):
     """A rule used to evaluate backend requests.
 
@@ -114,23 +109,3 @@ class Rule(models.Model):
     comment: models.TextField = models.TextField(default="", blank=True)
     created_at: models.DateTimeField = models.DateTimeField(auto_now_add=True)
     updated_at: models.DateTimeField = models.DateTimeField(auto_now=True)
-
-    def clean(self) -> None:
-        """Custom validation logic for the Rule model."""
-        if self.kind == RULE_KIND_HOSTNAME_AND_PATH_MATCH:
-            if not isinstance(self.value, dict):
-                raise ValidationError("The value field must be a JSON object.")
-
-            if hostnames := self.value.get("hostnames"):
-                if invalid_hostnames := [
-                    hostname for hostname in hostnames if not domain(hostname)
-                ]:
-                    raise ValidationError(
-                        f"Invalid hostname(s) in rule: {', '.join(invalid_hostnames)}"
-                    )
-
-            if paths := self.value.get("paths"):
-                if invalid_paths := [path for path in paths if is_valid_path(path)]:
-                    raise ValidationError(
-                        f"Invalid path(s) in rule: {', '.join([str(path) for path in invalid_paths])}"
-                    )
