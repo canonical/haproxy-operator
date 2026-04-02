0f1bbfd4d9269a6ce91ddb434061f08e776af90d
tphan025 <trung.thanh.phan@canonical.com>
2026-04-02 20:49:53 +0200

    ruff format

--- haproxy-route-policy/policy/migrations/0001_initial.py
+++ haproxy-route-policy/policy/migrations/0001_initial.py
@@ -4,25 +4,42 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     initial = True
 
-    dependencies = [
-    ]
+    dependencies = []
 
     operations = [
         migrations.CreateModel(
-            name='BackendRequest',
+            name="BackendRequest",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('relation_id', models.IntegerField()),
-                ('hostname_acls', models.JSONField(default=list)),
-                ('backend_name', models.TextField()),
-                ('paths', models.JSONField(default=list)),
-                ('port', models.IntegerField(null=True)),
-                ('status', models.TextField(choices=[('pending', 'pending'), ('accepted', 'accepted'), ('rejected', 'rejected')], db_index=True, default='pending')),
-                ('created_at', models.DateTimeField(auto_now_add=True)),
-                ('updated_at', models.DateTimeField(auto_now=True)),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("relation_id", models.IntegerField()),
+                ("hostname_acls", models.JSONField(default=list)),
+                ("backend_name", models.TextField()),
+                ("paths", models.JSONField(default=list)),
+                ("port", models.IntegerField(null=True)),
+                (
+                    "status",
+                    models.TextField(
+                        choices=[
+                            ("pending", "pending"),
+                            ("accepted", "accepted"),
+                            ("rejected", "rejected"),
+                        ],
+                        db_index=True,
+                        default="pending",
+                    ),
+                ),
+                ("created_at", models.DateTimeField(auto_now_add=True)),
+                ("updated_at", models.DateTimeField(auto_now=True)),
             ],
         ),
     ]
