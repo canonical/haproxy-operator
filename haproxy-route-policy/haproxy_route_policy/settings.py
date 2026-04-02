5c9441d3b8aa511cfb14c6009c82a4d6601af3a9
tphan025 <trung.thanh.phan@canonical.com>
2026-04-02 20:49:54 +0200

    minor fixes to settings

--- haproxy-route-policy/haproxy_route_policy/settings.py
+++ haproxy-route-policy/haproxy_route_policy/settings.py
@@ -18,16 +18,8 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DJANGO_DEBUG", "True") == "True"
+DEBUG = os.environ.get("DJANGO_DEBUG", "").lower() == "true"
 
 ALLOWED_HOSTS = []
 
