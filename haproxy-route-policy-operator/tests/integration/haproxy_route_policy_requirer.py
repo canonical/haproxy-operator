ee1b6d11ab8e4fa6f5395767c828768861917cef
tphan025 <trung.thanh.phan@canonical.com>
2026-04-10 11:52:38 +0200

    remove docstring

--- haproxy-route-policy-operator/tests/integration/haproxy_route_policy_requirer.py
+++ haproxy-route-policy-operator/tests/integration/haproxy_route_policy_requirer.py
@@ -23,7 +23,6 @@ class AnyCharm(AnyCharmBase):
     """haproxy-route requirer charm."""
 
     def __init__(self, *args, **kwargs):
-        # We don't need to include *args and *kwargs in the docstring here.
         """Initialize the requirer charm."""
         super().__init__(*args, **kwargs)
         self._haproxy_route_policy = HaproxyRoutePolicyRequirer(
