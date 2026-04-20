46ef45c847e17d094457067b81d74bd74a7b7e87
tphan025 <trung.thanh.phan@canonical.com>
2026-04-20 16:28:08 +0200

    update tests

--- tests/integration/test_haproxy_route_policy.py
+++ tests/integration/test_haproxy_route_policy.py
@@ -3,7 +3,6 @@
 
 """Integration tests for haproxy route policy."""
 
-import json
 import logging
 
 
@@ -18,33 +17,13 @@ TEST_HOSTNAME = "example.com"
 @pytest.mark.abort_on_fail
 def test_haproxy_route_policy(
     configured_application_with_tls: str,
-    haproxy_route_policy,
+    haproxy_route_policy: str,
     lxd_juju: jubilant.Juju,
-    any_charm_haproxy_route_deployer,
+    postgresql: str,
 ):
     """Test the HAProxy route policy integration."""
-    lxd_juju.integrate(
-        f"{configured_application_with_tls}:haproxy-route",
-        any_charm_haproxy_route_deployer,
-    )
+    lxd_juju.integrate(f"{haproxy_route_policy}:database", f"{postgresql}:database")
     lxd_juju.integrate(
         f"{configured_application_with_tls}:haproxy-route-policy",
         haproxy_route_policy,
     )
-    lxd_juju.run(
-        f"{any_charm_haproxy_route_deployer}/0",
-        "rpc",
-        {
-            "method": "update_relation",
-            "args": json.dumps(
-                [
-                    {
-                        "service": any_charm_haproxy_route_deployer,
-                        "ports": [80],
-                        "hostname": TEST_HOSTNAME,
-                    }
-                ]
-            ),
-        },
-    )
-    lxd_juju.wait(jubilant.all_active)
