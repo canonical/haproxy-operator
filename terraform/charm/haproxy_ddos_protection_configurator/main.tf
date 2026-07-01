c3df0ce30dfd0d3518044148a86528c39f8a9daf
tphan025 <trung.thanh.phan@canonical.com>
2026-07-01 10:08:36 +0200

    chore(terraform): move terraform block into terraform.tf per CC008

--- terraform/charm/haproxy_ddos_protection_configurator/main.tf
+++ terraform/charm/haproxy_ddos_protection_configurator/main.tf
@@ -1,16 +1,6 @@
 # Copyright 2025 Canonical Ltd.
 # See LICENSE file for licensing details.
 
-terraform {
-  required_providers {
-    juju = {
-      source  = "juju/juju"
-      version = "~> 1.0"
-    }
-  }
-  required_version = "~> 1.6"
-}
-
 resource "juju_application" "haproxy_ddos_protection_configurator" {
   name       = var.app_name
   model_uuid = var.model_uuid
