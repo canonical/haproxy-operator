f6992a225b303202cd0a144d53c7fff31734f4a9
Minh Nguyen <minh.nguyen@canonical.com>
2026-09-09 13:36:36 -0400

    Updating versioning for local charm lib

--- haproxy-operator/lib/charms/haproxy/v2/haproxy_route.py
+++ haproxy-operator/lib/charms/haproxy/v2/haproxy_route.py
@@ -157,7 +157,7 @@ LIBAPI = 2
 
 # Increment this PATCH version before using `charmcraft publish-lib` or reset
 # to 0 if you are raising the major API version
-LIBPATCH = 3
+LIBPATCH = 4
 
 logger = logging.getLogger(__name__)
 HAPROXY_ROUTE_RELATION_NAME = "haproxy-route"
